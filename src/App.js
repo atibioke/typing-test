a996600f4c0c81f8ccf8bad1dab5ceff5c4551df
Rasaq Ganiyu <eudoxusganium@gmail.com>
2022-08-14 19:12:01 +0100

    working on the functionality of typing test app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,152 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, useRef } from "react";
+import Timer from "./Helper/Timer";
+import WordsContainer from "./Helper/WordsContainer";
+import randomWords from "random-words";
+import "./main.css";
+const numOfWords = 250;
 
 function App() {
+  const [words, setWords] = useState([]);
+  const [currentInput, setCurrentInput] = useState("");
+  const [currentWordIndex, setCurrentWordIndex] = useState(0);
+  const [correctWords, setCorrectWords] = useState(0);
+  const [inCorrectWords, setInCorrectWords] = useState(0);
+  const [status, setStatus] = useState("pending");
+  const textInput = useRef(null)
+
+console.log(status, "App 1");
+
+  useEffect(() => {
+    setWords(generateWords());
+  }, []);
+
+  function generateWords() {
+    return new Array(numOfWords).fill(null).map(() => randomWords());
+  }
+
+  function handleKeyDown({ keyCode }) {
+    if (keyCode === 32) {
+      checkMatch();
+      setCurrentInput("");
+      setCurrentWordIndex(currentWordIndex + 1);
+    }
+  }
+
+  function checkMatch() {
+    const wordToCompare = words[currentWordIndex];
+    const matchStatus = wordToCompare === currentInput.trim();
+    matchStatus
+      ? setCorrectWords(correctWords + 1)
+      : setInCorrectWords(inCorrectWords + 1);
+    console.log({ matchStatus });
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Timer status={status} setStatus= {setStatus} />
+      <div className="input-container">
+        <input
+        ref={textInput}
+          type="text"
+          className="input"
+          onKeyDown={handleKeyDown}
+          value={currentInput}
+          onChange={(e) => setCurrentInput(e.target.value)}
+          disabled={status !== "started"}
+        />
+      </div>
+      { (status === "pending" || status === "finished") && <div className="button-container">
+        <button className="start-button" onClick={()=>setStatus("started")} >Start</button>
+      </div>}
+      {/* (
+        <div className="section">
+          <div className="card">
+            <div className="card-content">
+              <div className="content">
+                {words.map((word, index) => (
+                  <span key={index}>
+                    <span>
+                      {word.split("").map((letter, idx) => (
+                        <span key={idx}>{letter}</span>
+                      ))}
+                    </span>
+                    <span> </span>
+                  </span>
+                ))}
+              </div>
+            </div>
+          </div>
+        </div>
+      ) */}
+
+
+{/* //NEEDS ADJUSTMENT 1 */}
+
+{ status === "started" && 
+        <div className="section">
+          <div className="card">
+            <div className="card-content">
+              <div className="content">
+                {words.map((word, index) => (
+                  <span key={index}>
+                    <span>
+                      {word.split("").map((letter, idx) => (
+                        <span key={idx}>{letter}</span>
+                      ))}
+                    </span>
+                    <span> </span>
+                  </span>
+                ))}
+              </div>
+            </div>
+          </div>
+        </div> }
+      
+
+{/* //ENDING */}
+
+      {/* {status === "finished" && (
+        <div className="section">
+          <div className="columns">
+            <div className="column">
+              <p>Score:</p>
+              <p>{correctWords}</p>
+            </div>
+            <div className="column">
+              <div>Accuracy: </div>
+              <p>
+                {Math.round(
+                  (correctWords / (correctWords + inCorrectWords)) * 100
+                )}
+                %
+              </p>
+            </div>
+          </div>
+        </div>
+      )} */}
+   
+   {/* //NEEDS ADJUSTMENT 2*/}
+    
+       { status === "finished" &&  <div className="section">
+          <div className="columns">
+            <div className="column">
+              <p>Score:</p>
+              <p>{correctWords}</p>
+            </div>
+            <div className="column">
+              <div>Accuracy: </div>
+              <p>
+                {Math.round(
+                  (correctWords / (correctWords + inCorrectWords)) * 100
+                )}
+                %
+              </p>
+            </div>
+          </div>
+        </div>}
+      
+
+      {/* //ENDING */}
     </div>
   );
 }
---
+++ src/Helper/Timer.js
@@ -0,0 +1,62 @@
+import React, { useState, useEffect } from "react";
+
+function Timer({ status, setStatus }) {
+  //Timers Start
+  const [initialTime, setInitalTime] = useState("0");
+  const [minutes, setMinutes] = useState("0");
+  const [seconds, setSeconds] = useState("0");
+
+  useEffect(() => {
+    if (+initialTime && status === "started") {
+      let myInterval = setInterval(() => {
+        if (minutes !== 0) {
+          setSeconds(59);
+        }
+        if (seconds > 0) {
+          setSeconds(seconds - 1);
+        }
+        if (seconds === 0) {
+          if (minutes === 0) {
+            setStatus("finished");
+            return clearInterval(myInterval);
+          } else {
+            setMinutes((prev) => prev - 1);
+            setSeconds(59);
+          }
+        }
+      }, 1000);
+      return () => {
+        clearInterval(myInterval);
+      };
+    }
+  });
+
+  useEffect(() => {
+    if (+initialTime && status === "started") {
+      setMinutes(initialTime - 1);
+    }
+  }, [status, initialTime]);
+  //minutes ENDS
+
+  return (
+    <div className="minutes">
+      <input
+        value={initialTime}
+        className="minutes-input"
+        placeholder="Input min"
+        onChange={(e) => {
+          setInitalTime(e.target.value);
+        }}
+        disabled={status === "started"}
+      ></input>
+
+      {minutes === 0 && seconds === 0 ? null : (
+        <h1>
+          {minutes}:{seconds < 10 ? `0${seconds}` : seconds}
+        </h1>
+      )}
+    </div>
+  );
+}
+
+export default Timer;
